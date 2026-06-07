84cd7b86bd52f84d36528ca87340a48a128148e6
MarcoTeco1 <mtullio22@gmail.com>
2026-06-07 12:18:32 -0300

    Conclusão e finalização da parte 6

--- unileste-sistemasmoveis/dt-money-app/src/context/transaction.context.tsx
+++ unileste-sistemasmoveis/dt-money-app/src/context/transaction.context.tsx
@@ -1,18 +1,45 @@
-import { CreateTransactionRequest } from '@/shared/interfaces/http/createTransactionRequest'
+import { CreateTransactionRequest, UpdateTransactionRequest } from '@/shared/interfaces/http/createTransactionRequest'
+import { Pagination } from '@/shared/interfaces/http/get-transactions-request'
 import { TransactionCategory } from '@/shared/interfaces/http/transaction-category-response'
+import { TotalTransactions } from '@/shared/interfaces/total-transactions' 
+import { Transaction } from '@/shared/interfaces/transaction'
 import * as TransactionService from '@/shared/services/dtMoney/transaction.service'
 import {
   FC,
   PropsWithChildren,
   createContext,
+  useCallback,
   useContext,
   useState,
 } from 'react'
 
+interface FetchTransactionsParams {
+  page: number
+}
+
+interface Loadings {
+  initial: boolean
+  refresh: boolean
+  loadMore: boolean
+}
+
+interface HandleLoadingsParams {
+  key: keyof Loadings
+  value: boolean
+}
+
 type TransactionContextType = {
   fetchCategories: () => Promise<void>
-  createTransaction: (transaction: CreateTransactionRequest) => Promise<void> 
+  fetchTransactions: (params: FetchTransactionsParams) => Promise<void>
+  refreshTransactions: () => Promise<void>
+  loadMoreTransactions: () => Promise<void>
+  createTransaction: (transaction: CreateTransactionRequest) => Promise<void>
+  updateTransaction: (transaction: UpdateTransactionRequest) => Promise<void> 
   categories: TransactionCategory[]
+  transactions: Transaction[]
+  totalTransactions: TotalTransactions
+  loadings: Loadings 
+  handleLoadings: (params: HandleLoadingsParams) => void 
 }
 
 export const TransactionContext = createContext({} as TransactionContextType)
@@ -21,26 +48,122 @@ export const TransactionContextProvider: FC<PropsWithChildren> = ({
   children,
 }) => {
   const [categories, setCategories] = useState<TransactionCategory[]>([])
+  const [transactions, setTransactions] = useState<Transaction[]>([])
+  const [totalTransactions, setTotalTransactions] = useState<TotalTransactions>({
+    expense: 0,
+    revenue: 0,
+    total: 0,
+  })
+  
+  const [loadings, setLoadings] = useState<Loadings>({
+    initial: false,
+    refresh: false,
+    loadMore: false,
+  })
+
+  const [pagination, setPagination] = useState<Pagination>({
+    page: 1,
+    perPage: 15,
+    totalRows: 0,
+    totalPages: 0,
+  })
+
+  const handleLoadings = ({ key, value }: HandleLoadingsParams) => {
+    setLoadings((prevValues) => ({
+      ...prevValues,
+      [key]: value,
+    }))
+  }
 
   const fetchCategories = async () => {
-    const categoriesResponse =
-      await TransactionService.getTransactionCategories()
-
+    const categoriesResponse = await TransactionService.getTransactionCategories()
     setCategories(categoriesResponse)
   }
+  const fetchTransactions = useCallback(
+    async ({ page }: FetchTransactionsParams) => {
+      const transactionResponse = await TransactionService.getTransactions({
+        page,
+        perPage: pagination.perPage,
+      })
+
+      if (page === 1) {
+        setTransactions(transactionResponse.data)
+      } else {
+        setTransactions((prevState) => [
+          ...prevState,
+          ...transactionResponse.data,
+        ])
+      }
+
+      setTotalTransactions(transactionResponse.totalTransactions)
+
+      setPagination({
+        ...pagination,
+        page,
+        totalRows: transactionResponse.totalRows,
+        totalPages: transactionResponse.totalPages,
+      })
+    },
+    [pagination]
+  )
+
+  const loadMoreTransactions = useCallback(async () => {
+    if (loadings.loadMore || pagination.page >= pagination.totalPages) {
+      return
+    }
+
+    await fetchTransactions({
+      page: pagination.page + 1,
+    })
+  }, [loadings.loadMore, pagination, fetchTransactions])
+
+  const refreshTransactions = useCallback(async () => {
+    const { page, perPage } = pagination
+
+    const transactionResponse = await TransactionService.getTransactions({
+      page: 1,
+      perPage: page * perPage,
+    })
+
+    setTransactions(transactionResponse.data)
+    setTotalTransactions(transactionResponse.totalTransactions)
+
+    setPagination({
+      ...pagination,
+      page,
+      totalRows: transactionResponse.totalRows,
+      totalPages: transactionResponse.totalPages,
+    })
+  }, [pagination])
 
   const createTransaction = async (
     transaction: CreateTransactionRequest
   ) => {
     await TransactionService.createTransaction(transaction)
+    await refreshTransactions()
+  }
+
+  const updateTransaction = async (
+    transaction: UpdateTransactionRequest
+  ) => {
+    await TransactionService.updateTransaction(transaction)
+    await refreshTransactions()
   }
 
   return (
     <TransactionContext.Provider
       value={{
         categories,
+        transactions,
+        totalTransactions,
+        loadings, 
+        handleLoadings, 
         fetchCategories,
-        createTransaction, 
+        fetchTransactions,
+        refreshTransactions,
+        loadMoreTransactions,
+        createTransaction,
+        updateTransaction,
       }}
     >
       {children}
--- unileste-sistemasmoveis/dt-money-backend/src/infra/web/config/cors.ts
+++ unileste-sistemasmoveis/dt-money-backend/src/infra/web/config/cors.ts
@@ -2,5 +2,9 @@ import { FastifyInstance } from "fastify";
 import cors from "@fastify/cors";
 
 export const register = async (fastify: FastifyInstance) => {
-  await fastify.register(cors);
+  await fastify.register(cors, {
+    origin: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+  });
 };
